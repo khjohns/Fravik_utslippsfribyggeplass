7e2a9f0dc1bbd36fdb4e23f6205d239b99971341
khjohns <kasper.johns@gmail.com>
2025-11-07 15:04:02 +0100

    Update heading and adjust main padding

--- App.tsx
+++ App.tsx
@@ -22,9 +22,9 @@ const App: React.FC = () => {
         }}
       />
 
-      <h1 className="sr-only">Søknad om fravik - Krav til utslippsfri byggeplass</h1>
+      <h1 className="sr-only">Søknad om fravik - Oslo kommunes krav til utslippsfri byggeplasser</h1>
 
-      <main className="pt-24 pb-8 sm:pb-12">
+      <main className="pt-32 pb-8 sm:pb-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {appState === 'start' && <StartScreen onStart={handleStartApplication} />}
           {appState === 'form' && <MainForm />}
